d3501d9afe4fdd9c65395bc394b7f33ec8ff7d54
Suzy Adel Wahba <128175020+SuzyAdel@users.noreply.github.com>
2025-06-11 13:07:05 +0300

    Playing the shatter animation and sound on trigger

---
+++ RockProjectile.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class RockProjectile : MonoBehaviour
+
+{
+    public AudioClip shatterSound;             
+    private AudioSource audioSource;           
+    private Animator animator; // Given animator on the rock
+
+
+    private bool hasShattered = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter(Collider other) // called when the rock collides with another object
+     
+    {
+        if (hasShattered) return;  // Avoid double triggering, happens when the rock hits multiple objects
+        hasShattered = true;
+
+        // Play shatter animation
+        if (animator != null)
+            animator.SetTrigger("Shatter");
+
+        // Play shatter sound
+        if (audioSource != null && shatterSound != null)
+            audioSource.PlayOneShot(shatterSound);
+
+        // Optional: disable collider to prevent further hits
+        Collider col = GetComponent<Collider>();
+        if (col != null)
+            col.enabled = false;
+
+        // Destroy after 1 second to allow animation and sound to play
+        Destroy(gameObject, 1f);
+    }
+
+// Update is called once per frame
+void Update()
+    {
+        
+    }
+}
